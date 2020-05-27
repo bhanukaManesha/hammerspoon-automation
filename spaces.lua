d2e45c4e2c6e10b0204d8ecf7f8d9c97d2342bef
Bhanuka Gamage <bhanukamgamage@gmail.com>
2020-05-28 04:32:45 +0800

    add logic to shift between spaces

---
+++ spaces.lua
@@ -0,0 +1,55 @@
+-- Code from https://github.com/kev-zheng/hammerspoon
+
+local hotkey = require "hs.hotkey"
+local window = require "hs.window"
+local spaces = require "hs._asm.undocumented.spaces" 
+
+local function getGoodFocusedWindow(nofull)
+   local win = window.focusedWindow()
+   if not win or not win:isStandard() then return end
+   if nofull and win:isFullScreen() then return end
+   return win
+end 
+
+local function errorScreen(screen)
+  hs.sound.getByName("Funk"):play()
+end 
+
+local function moveWindowOneSpace(dir,switch)
+   local win = getGoodFocusedWindow(true)
+   if not win then return end
+   local screen=win:screen()
+   local uuid=screen:spacesUUID()
+   local userSpaces=spaces.layout()[uuid]
+   local thisSpace=win:spaces() -- first space win appears on
+   if not thisSpace then return else thisSpace=thisSpace[1] end
+   local last=nil
+   local skipSpaces=0
+   for _, spc in ipairs(userSpaces) do
+      if spaces.spaceType(spc)~=spaces.types.user then -- skippable space
+	 skipSpaces=skipSpaces+1
+      else 			-- A good user space, check it
+	 if last and
+	    (dir=="left"  and spc==thisSpace) or
+	    (dir=="right" and last==thisSpace)
+	 then
+	    win:spacesMoveTo(dir=="left" and last or spc)
+	    if switch then
+         --  switchSpace(skipSpaces+1,dir)
+	       win:focus()
+	    end
+	    return
+	 end
+	 last=spc	 -- Haven't found it yet...
+	 skipSpaces=0
+      end 
+   end
+   errorScreen(screen)   -- Shouldn't get here, so no space found
+end
+
+local commandoptionshift = {"command","alt","⇧"}
+
+hotkey.bind(commandoptionshift, "left", nil,
+	    function() moveWindowOneSpace("left", true) end)
+hotkey.bind(commandoptionshift, "right",nil,
+        function() moveWindowOneSpace("right", true) end)
